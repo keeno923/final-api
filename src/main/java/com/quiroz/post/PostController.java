3929a5d8407841f50704a943ee8f50c455b921ab
keeno923 <keenoquiroz9@gmail.com>
2025-05-18 21:53:37 +0800

    second commit

--- src/main/java/com/quiroz/post/PostController.java
+++ src/main/java/com/quiroz/post/PostController.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/quiroz/posts")
-@CrossOrigin(origins = {"http://localhost:5173"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://final-ui-w8jw.onrender.com"})
 public class PostController {
 
     @Autowired
